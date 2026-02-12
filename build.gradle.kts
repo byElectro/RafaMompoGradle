42117a0d9e7340bb510e9ff010318e5196f5f7b5
Rafa Tafol <rafatafol26@gmail.com>
2026-02-12 16:07:51 +0100

    Subido el bloque de gradle completo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.rafamompo.tema4gradle"
@@ -13,8 +14,29 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
+    implementation("dev.langchain4j:langchain4j-open-ai")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("com.rafamompo.tema4gradle.Main")
+}
+
+tasks.register<Exec>("ollamaVersion") {
+    commandLine("powershell", "-NoProfile", "-Command", "ollama --version")
+}
+
+tasks.register<Exec>("ollamaPs") {
+    commandLine("powershell", "-NoProfile", "-Command", "ollama ps")
+}
+
+tasks.register("llmInfo") {
+    dependsOn("ollamaVersion", "ollamaPs")
+    doLast {
+        println("Demo finalizada")
+    }
 }
--- src/main/java/com/rafamompo/tema4gradle/Main.java
+++ src/main/java/com/rafamompo/tema4gradle/Main.java
@@ -1,17 +1,50 @@
 package com.rafamompo.tema4gradle;
 
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        final String TOKEN = "";
+
+        var modeloIA1 = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("llama3.1:8b")
+                .build();
+
+        var modeloIA2 = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("llama3.1:8b")
+                .build();
+
+        //Ejemplo de historial:
+        List<ChatMessage> historialIA1 = new ArrayList<>();
+        historialIA1.add(new UserMessage("Eres un comentarista profesional de videojuegos."));
+        AiMessage mensajeIA1 = modeloIA1.chat(historialIA1).aiMessage();
+        String pregunta = mensajeIA1.text();
+
+        // El modeloloIA2 recibe la pregunta formulada por el
+        // modeloIA1, generando una respuesta y la segunda IA extrae el texto generado por el modeloIA2
+        String respuestaIA2 = modeloIA2.chat(List.of(new UserMessage(pregunta))).aiMessage().text();
+
+        //Interaccion entre IAS:
+        List<ChatMessage> historialIA2 = new ArrayList<>();
+        historialIA1.add(new SystemMessage("Eres un experto en física cuántica."));
+
+        System.out.println("IA1 pregunta: " + pregunta);
+        System.out.println("IA2 responde: " + respuestaIA2);
+
+
     }
 }
